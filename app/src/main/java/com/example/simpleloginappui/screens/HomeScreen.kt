a016c62a3f82bf2db8853913cbee4130086f84da
jlpmartinss <jm8244936@gmail.com>
2024-11-07 11:11:51 +0000

    greetings for home screen

--- app/src/main/java/com/example/simpleloginappui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/simpleloginappui/screens/HomeScreen.kt
@@ -1,9 +1,49 @@
 package com.example.simpleloginappui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simpleloginappui.ui.theme.PrimaryGreen
+import com.example.simpleloginappui.ui.theme.PrimaryViolet
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    0f to PrimaryViolet,
+                    1f to PrimaryGreen
+                )
+            )
+    ) {
+        Greetings()
 
+    }
+}
+
+@Composable
+fun Greetings(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(text = "Congratulations!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPrev() {
+    HomeScreen()
 }
