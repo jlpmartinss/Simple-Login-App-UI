7d8bc03db061acc7255f531c258c2ea96b617d56
jlpmartinss <jm8244936@gmail.com>
2024-11-07 11:13:54 +0000

    refactor registration, using a authentication template because is used on the login screen to

--- app/src/main/java/com/example/simpleloginappui/screens/Registration.kt
+++ app/src/main/java/com/example/simpleloginappui/screens/Registration.kt
@@ -1,9 +1,43 @@
 package com.example.simpleloginappui.screens
 
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.simpleloginappui.R
+import com.example.simpleloginappui.components.AuthenticationScreenTemplate
+import com.example.simpleloginappui.ui.theme.PrimaryViolet
+import com.example.simpleloginappui.ui.theme.PrimaryVioletDark
+import com.example.simpleloginappui.ui.theme.PrimaryVioletLight
 
 @Composable
-fun RegistrationScreen(modifier: Modifier = Modifier) {
-
+fun RegistrationScreen(
+    modifier: Modifier = Modifier,
+    onRegisterClicked: () -> Unit,
+    onLoginClicked: () -> Unit
+) {
+    AuthenticationScreenTemplate(
+        modifier = modifier,
+        backgroundGradient = arrayOf(
+            0f to PrimaryViolet,
+            1f to PrimaryVioletDark
+        ),
+        imgRes = R.drawable.img_coder_w,
+        title = "Hi there!",
+        subtitle = "Let's Get Started",
+        mainActionButtonTitle = "Create an Account",
+        secondaryActionButtonTitle = "Log In",
+        mainActionButtonColors = ButtonDefaults.buttonColors(
+            containerColor = PrimaryVioletDark,
+            contentColor = Color.White
+        ),
+        secondaryActionButtonColors = ButtonDefaults.buttonColors(
+            containerColor = PrimaryVioletLight,
+            contentColor = Color.White
+        ),
+        actionButtonShadow = PrimaryVioletDark,
+        onMainActionButtonClicked = onRegisterClicked,
+        onSecondaryActionButtonClicked = onLoginClicked,
+    )
 }
+
