ede5dfe11b8fa4eee0732dd034938f1003b27883
jlpmartinss <jm8244936@gmail.com>
2024-11-07 11:33:16 +0000

    center the text in the middle of the screen and and a button

--- app/src/main/java/com/example/simpleloginappui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/simpleloginappui/screens/HomeScreen.kt
@@ -4,22 +4,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.simpleloginappui.components.ActionButton
 import com.example.simpleloginappui.ui.theme.PrimaryGreen
+import com.example.simpleloginappui.ui.theme.PrimaryGreenDark
+import com.example.simpleloginappui.ui.theme.PrimaryPinkDark
 import com.example.simpleloginappui.ui.theme.PrimaryViolet
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    onRestartFlowClicked: () -> Unit
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .background(
                 Brush.verticalGradient(
@@ -28,22 +40,46 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 )
             )
     ) {
+        Spacer(modifier = Modifier.weight(1f))
         Greetings()
-
+        Spacer(modifier = Modifier.weight(1f))
+        ActionButton(
+            onClick = { /*TODO*/ },
+            text = "Press to restart",
+            colors = ButtonDefaults.buttonColors(
+                containerColor = PrimaryGreenDark,
+                contentColor = Color.White
+            ),
+            shadowColor = PrimaryPinkDark,
+            isNavigationArrowVisible = false,
+            modifier = Modifier.padding(24.dp)
+        )
     }
+
 }
 
 @Composable
 fun Greetings(modifier: Modifier = Modifier) {
     Row(
+        modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text(text = "Congratulations!")
+        Text(
+            text = "Congratulations!",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Text(
+            text = "\uD83E\uDD73",
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun HomeScreenPrev() {
-    HomeScreen()
+    HomeScreen(onRestartFlowClicked = {})
 }
